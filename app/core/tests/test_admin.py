8240e586564dbb22d9f563026e68bfda1819c5e3
Suraj Gusain <suraj.gusain15@gmail.com>
2025-10-31 18:04:42 +0530

    added tests for django admin users list

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.test import Client
+from django.test import TestCase
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='test123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='test123',
+            name='Test user'
+        )
+
+    def test_users_list(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
