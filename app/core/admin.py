d668143b629994ce45b056731cb01012e5943b37
Suraj Gusain <suraj.gusain15@gmail.com>
2025-11-01 12:32:30 +0530

    added django admin users list visible after django admin login

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
